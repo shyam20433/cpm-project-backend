4484e8067602e18f29fc8b0c226dcc76397b0c24
Shyamsundar S S <shyamsundar93424@gmail.com>
2026-07-23 16:50:49 +0530

    added JWT middleware

--- app/Middleware/JwtAuth.ts
+++ app/Middleware/JwtAuth.ts
@@ -1,36 +1,79 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
-import jwt from "jsonwebtoken";
 import Env from "@ioc:Adonis/Core/Env";
+import jwt from "jsonwebtoken";
+
+import Endpoint from "App/Models/Endpoint";
+import AssignedPermission from "App/Models/AssignedPermission";
+import AssignedEndpoint from "App/Models/AssignedEndpoint";
+
 export default class JwtAuth {
   public async handle(
     { request, response }: HttpContextContract,
     next: () => Promise<void>
   ) {
-    // code for middleware goes here. ABOVE THE NEXT CALL
-
     const authHeader = request.header("authorization");
+
     if (!authHeader) {
       return response.unauthorized({
-        success: false,
-        message: "Authorization heading is missing",
+        message: "Token Missing",
       });
     }
-    if (!authHeader.startsWith("Bearer ")) {
-      return response.unauthorized({
-        success: false,
-        message: "Invalid Authorization header ",
-      });
-    }
-    const token = authHeader.split(" ")[1];
-    try {
-      jwt.verify(token, Env.get("APP_KEY"))
 
-      await next();
-    } catch (error) {
+    const token = authHeader.replace("Bearer ", "");
+
+    let payload: any;
+
+    try {
+      payload = jwt.verify(token, Env.get("APP_KEY"));
+    } catch {
       return response.unauthorized({
-        success: false,
-        message: "Invalid or Expired Token",
+        message: "Invalid Token",
       });
     }
+
+    const roleKey = payload.roleKey;
+
+    const method = request.method();
+    const pattern = request.url();
+    console.log(request.url());
+    console.log(request.method());
+    const endpoints = await Endpoint.query().where("method", method);
+    console.log(endpoints)
+
+    const endpoint = endpoints.find((e) => {
+      const regex = new RegExp("^" + e.route.replace(/:[^/]+/g, "[^/]+") + "$");
+
+      return regex.test(pattern);
+    });
+
+    if (!endpoint) {
+      return response.notFound({
+        message: "Endpoint not found",
+      });
+    }
+    const permissions = await AssignedPermission.query().where(
+      "roleKey",
+      roleKey
+    );
+
+
+    const permissionKeys = permissions.map(
+      (permission) => permission.permissionKey
+    );
+    console.log(permissionKeys)
+
+    const access = await AssignedEndpoint.query()
+      .where("endpointId", endpoint.id)
+      .whereIn("permissionKey", permissionKeys)
+      .first();
+    console.log(access)
+
+    if (!access) {
+      return response.forbidden({
+        message: "Permission Denied",
+      });
+    }
+
+    await next();
   }
 }
