8e4774792bcd7584437285e44dd54312a5953782
Shyamsundar S S <shyamsundar93424@gmail.com>
2026-07-24 12:57:16 +0530

    sequential query to joins

--- app/Middleware/JwtAuth.ts
+++ app/Middleware/JwtAuth.ts
@@ -3,12 +3,11 @@ import Env from "@ioc:Adonis/Core/Env";
 import jwt from "jsonwebtoken";
 
 import Endpoint from "App/Models/Endpoint";
-import AssignedPermission from "App/Models/AssignedPermission";
 import AssignedEndpoint from "App/Models/AssignedEndpoint";
 
 export default class JwtAuth {
   public async handle(
-    { request, response }: HttpContextContract,
+    { request, response, route }: HttpContextContract,
     next: () => Promise<void>
   ) {
     const authHeader = request.header("authorization");
@@ -32,41 +31,27 @@ export default class JwtAuth {
     }
 
     const roleKey = payload.roleKey;
-
     const method = request.method();
-    const pattern = request.url();
-    console.log(request.url());
-    console.log(request.method());
-    const endpoints = await Endpoint.query().where("method", method);
-    console.log(endpoints)
-
-    const endpoint = endpoints.find((e) => {
-      const regex = new RegExp("^" + e.route.replace(/:[^/]+/g, "[^/]+") + "$");
-
-      return regex.test(pattern);
-    });
+    const pattern = route!.pattern;
+    const endpoint = await Endpoint.query()
+      .where("method", method)
+      .where("route", pattern)
+      .first();
 
     if (!endpoint) {
       return response.notFound({
         message: "Endpoint not found",
       });
     }
-    const permissions = await AssignedPermission.query().where(
-      "roleKey",
-      roleKey
-    );
-
-
-    const permissionKeys = permissions.map(
-      (permission) => permission.permissionKey
-    );
-    console.log(permissionKeys)
 
     const access = await AssignedEndpoint.query()
-      .where("endpointId", endpoint.id)
-      .whereIn("permissionKey", permissionKeys)
+      .join(
+        "assigned_permissions","assigned_permissions.permissionKey","assigned_endpoints.permissionKey"
+      )
+      .where("assigned_permissions.roleKey", roleKey)
+      .where("assigned_endpoints.endpointId", endpoint.id)
+      .select("assigned_endpoints.*")
       .first();
-    console.log(access)
 
     if (!access) {
       return response.forbidden({
